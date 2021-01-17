37930486a2fe65a3f88a7910dc1fddee648a59ad
Ved Prakash <35768588+Vedprakash12@users.noreply.github.com>
2021-01-17 19:03:50 +0530

    delete operation is done 1.1

--- src/main/java/com/infinty8/portfolio/MyPortfolioApplication.java
+++ src/main/java/com/infinty8/portfolio/MyPortfolioApplication.java
@@ -2,9 +2,10 @@ package com.infinty8.portfolio;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class MyPortfolioApplication {
+public class MyPortfolioApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MyPortfolioApplication.class, args);
